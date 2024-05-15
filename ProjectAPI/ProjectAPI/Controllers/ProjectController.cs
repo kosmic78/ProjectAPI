28cccb9c4ac4daf7fbfdd28ee201ad77c8892eb1
Cristian Gabor <92177475+kosmic78@users.noreply.github.com>
2024-05-15 12:02:45 +0300

    Add files via upload

---
+++ ProjectAPI/ProjectAPI/Controllers/ProjectController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectAPI.Data;
+
+namespace ProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ProjectController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Project>>> GetSuperHeroes()
+        {
+            return Ok(await _context.Projects.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateProject(Project project)
+        {
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Projects.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Project>>> UpdateProject(Project project)
+        {
+            var dbProject = await _context.Projects.FindAsync(project.Id);
+            if (dbProject == null)
+                return BadRequest("Project not found.");
+
+            dbProject.Name = project.Name;
+            dbProject.FirstName = project.FirstName;
+            dbProject.LastName = project.LastName;
+            dbProject.Place = project.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Projects.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Project>>> DeleteProject(int id)
+        {
+            var dbProject = await _context.Projects.FindAsync(id);
+            if (dbProject == null)
+                return BadRequest("Project not found.");
+
+            _context.Projects.Remove(dbProject);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Projects.ToListAsync());
+        }
+    }
+}
---
+++ ProjectAPI/ProjectAPI/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperHeroAPI
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Thumbnail { get; set; } = string.Empty;
+        public string Website { get; set; } = string.Empty;
+
+    }
+}
